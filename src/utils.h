28faaffeb59ba0735386cdbcf25fe9682978bf22
Marco Bortolin <barotto@gmail.com>
2015-04-13 13:27:02 +0200

    Add missing source file

---
+++ src/utils.h
@@ -0,0 +1,32 @@
+/*
+ * 	Copyright (c) 2015  Marco Bortolin
+ *
+ *	This file is part of IBMulator
+ *
+ *  IBMulator is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	IBMulator is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with IBMulator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef IBMULATOR_UTILS_H
+#define IBMULATOR_UTILS_H
+
+void str_replace_all(std::string &_str, const std::string &_search, const std::string &_replace)
+{
+	std::string::size_type i = _str.find(_search);
+    while(i != std::string::npos) {
+        _str.replace(i, _search.length(), _replace);
+        i = _str.find(_search, i+_replace.length());
+    }
+}
+
+#endif
