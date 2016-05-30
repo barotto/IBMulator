9c74bc47d68deb6e3135d9ef8806438140d5e2e6
Marco Bortolin <barotto@gmail.com>
2016-05-30 19:48:27 +0200

    Add CRT convergence to realistic mode fragment shader

--- share/ibmulator/gui/shaders/fb-realistic.fs
+++ share/ibmulator/gui/shaders/fb-realistic.fs
@@ -21,6 +21,8 @@ const vec3 brt_tint = vec3(1.0,1.0,1.5);
 // 1.0/8.0 = extreme
 const vec2 warp = vec2(1.0/80.0, 1.0/64.0); 
 
+// CRT convergence (horiz. chromatic aberrations)
+const float convergence = 0.0006;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -37,7 +39,14 @@ vec2 Warp(vec2 pos, vec2 warp_amount)
 void main()
 {
 	vec2 pos = Warp(UV, warp);
-	vec3 vga_color = FetchTexel(iVGAMap, pos).rgb;
+	float chabx = clamp(abs(pos.x*2.0 - 1.0) + 0.5, 0.0, 1.0) * convergence;
+	vec2 rOffset = vec2(0.0*chabx,0.0);
+	vec2 gOffset = vec2(1.0*chabx,0.0);
+	vec2 bOffset = vec2(2.0*chabx,0.0);
+    float rValue = FetchTexel(iVGAMap, pos - rOffset).r;  
+    float gValue = FetchTexel(iVGAMap, pos - gOffset).g;
+    float bValue = FetchTexel(iVGAMap, pos - bOffset).b;  
+	vec3 vga_color = vec3(rValue, gValue, bValue);
 	vga_color = BrightnessSaturationContrast(vga_color, iBrightness, brt_tint, iSaturation, iContrast);
 	vec3 reflection = vec3(Luma(texture(iReflectionMap, ReflectionUV).rgb)) * iAmbientLight;
 	float vga_luma = Luma(vga_color);
