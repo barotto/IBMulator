09544ff03f7f41cd83e6f622bf43d909ebbf75ed
Marco Bortolin <barotto@gmail.com>
2023-03-29 16:50:40 +0200

    Fix possible deadlock or crash when restoring an invalid state

--- src/program.cpp
+++ src/program.cpp
@@ -291,19 +291,19 @@ void Program::restore_state(
 		m_machine->cmd_restore_state(sstate->state(), ms_lock, ms_cv);
 		ms_cv.wait(restore_lock);
 
+		// we need to pause the syslog because it'll use the GUI otherwise
+		g_syslog.cmd_pause_and_signal(ms_lock, ms_cv);
+		ms_cv.wait(restore_lock);
+		m_gui->config_changed(false);
+		m_gui->sig_state_restored();
+		g_syslog.cmd_resume();
+
 		if(sstate->state().m_last_restore) {
 			m_mixer->sig_config_changed(ms_lock, ms_cv);
 			ms_cv.wait(restore_lock);
 
 			m_mixer->cmd_restore_state(sstate->state(), ms_lock, ms_cv);
 			ms_cv.wait(restore_lock);
-
-			// we need to pause the syslog because it'll use the GUI otherwise
-			g_syslog.cmd_pause_and_signal(ms_lock, ms_cv);
-			ms_cv.wait(restore_lock);
-			m_gui->config_changed(false);
-			m_gui->sig_state_restored();
-			g_syslog.cmd_resume();
 
 			// mixer resume cmd is issued by the machine
 			m_machine->cmd_resume(false);
