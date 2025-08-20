fe24c62b154194fb7136e5c8c598d0522860ff1f
Marco Bortolin <barotto@gmail.com>
2025-08-20 17:37:36 +0200

    Bump savestate version

--- src/state_record.cpp
+++ src/state_record.cpp
@@ -227,6 +227,7 @@ std::string StateRecord::get_version_to_release_string(unsigned _version)
 		case 2: return "0.15-0.16";
 		case 3: return "0.17";
 		case 4: return "0.18";
+		case 5: return "0.19";
 		default: return "pre-0.14";
 	}
 }
